8778f349ce53035ed684a341cc82b87342632bb9
Muhammad Ahsan bin Abdul Aziz <maahsan36@gmail.com>
2024-10-30 22:40:02 +0500

    first commit

---
+++ Amoozesh/AmoozeshApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmoozeshApp.swift
+//  Amoozesh
+//
+//  Created by Apple on 30/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct AmoozeshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
